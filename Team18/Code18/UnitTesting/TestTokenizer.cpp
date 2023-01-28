72c8b9dd623aa224da7534fcca1eb3a109443e81
Imranr2 <imranr2@msn.com>
2023-01-28 14:03:01 +0800

    Add unit tests

--- Team18/Code18/UnitTesting/TestTokenizer.cpp
+++ Team18/Code18/UnitTesting/TestTokenizer.cpp
@@ -10,7 +10,7 @@ using Microsoft::VisualStudio::CppUnitTestFramework::Assert;
 namespace UnitTesting {
     TEST_CLASS(TestTokenizer) {
         public:
-        TEST_METHOD(TestTokenizerTokenize) {
+        TEST_METHOD(TestTokenizerTokenizePattern) {
             std::stringstream srcStream;
             srcStream << "assign a;\n";
             srcStream << "Select a pattern a(\"normSq\", _\"cenX * cenX\"_)";
@@ -39,6 +39,67 @@ namespace UnitTesting {
                 { spa::TOKEN_CLOSE_BRACKET, ")" },
             });
             Assert::IsTrue(matchResult);
+        }
+
+        TEST_METHOD(TestTokenizerTokenizeSuchThat) {
+          std::stringstream srcStream;
+          srcStream << "variable v;\n";
+          srcStream << "Select v such that Modifies (6, v)";
+          spa::Tokenizer tokenizer;
+          spa::Stream<spa::Token> tokenStream = tokenizer.tokenize(srcStream);
+          bool matchResult = tokenStream.match({
+            { spa::TOKEN_NAME, "variable" },
+            { spa::TOKEN_NAME, "v" },
+            { spa::TOKEN_SEMICOLON, ";" },
+            { spa::TOKEN_NAME, "Select" },
+            { spa::TOKEN_NAME, "v" },
+            { spa::TOKEN_NAME, "such" },
+            { spa::TOKEN_NAME, "that" },
+            { spa::TOKEN_NAME, "Modifies" },
+            { spa::TOKEN_OPEN_BRACKET, "(" },
+            { spa::TOKEN_INTEGER, "6" },
+            { spa::TOKEN_COMMA, "," },
+            { spa::TOKEN_NAME, "v" },
+            { spa::TOKEN_CLOSE_BRACKET, ")" },
+            });
+          Assert::IsTrue(matchResult);
+        }
+
+        TEST_METHOD(TestTokenizerTokenizeSuchThatAndPattern) {
+          std::stringstream srcStream;
+          srcStream << "assign a; while w;\n";
+          srcStream << "Select w such that Parent* (w, a) pattern a (\"count\", _)";
+          spa::Tokenizer tokenizer;
+          spa::Stream<spa::Token> tokenStream = tokenizer.tokenize(srcStream);
+          bool matchResult = tokenStream.match({
+            { spa::TOKEN_NAME, "assign" },
+            { spa::TOKEN_NAME, "a" },
+            { spa::TOKEN_SEMICOLON, ";" },
+            { spa::TOKEN_NAME, "while" },
+            { spa::TOKEN_NAME, "w" },
+            { spa::TOKEN_SEMICOLON, ";" },
+            { spa::TOKEN_NAME, "Select" },
+            { spa::TOKEN_NAME, "w" },
+            { spa::TOKEN_NAME, "such" },
+            { spa::TOKEN_NAME, "that" },
+            { spa::TOKEN_NAME, "Parent" },
+            { spa::TOKEN_MULTIPLY, "*" },
+            { spa::TOKEN_OPEN_BRACKET, "(" },
+            { spa::TOKEN_NAME, "w" },
+            { spa::TOKEN_COMMA, "," },
+            { spa::TOKEN_NAME, "a" },
+            { spa::TOKEN_CLOSE_BRACKET, ")" },
+            { spa::TOKEN_NAME, "pattern" },
+            { spa::TOKEN_NAME, "a" },
+            { spa::TOKEN_OPEN_BRACKET, "(" },
+            { spa::TOKEN_DOUBLE_QUOTES, "\"" },
+            { spa::TOKEN_NAME, "count" },
+            { spa::TOKEN_DOUBLE_QUOTES, "\"" },
+            { spa::TOKEN_COMMA, "," },
+            { spa::TOKEN_UNDERSCORE, "_" },
+            { spa::TOKEN_CLOSE_BRACKET, ")"}
+            });
+          Assert::IsTrue(matchResult);
         }
     };
 }  // namespace UnitTesting
