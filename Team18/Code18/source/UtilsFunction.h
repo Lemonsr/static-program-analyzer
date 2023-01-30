f80865045fdb5bf1618b63d123c0da9db9f09fe7
LeeMingDe <58969901+LeeMingDe@users.noreply.github.com>
2023-01-30 16:26:32 +0800

    Create util functions to change token to postfix

---
+++ Team18/Code18/source/UtilsFunction.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "Token.h"
+
+class UtilsFunction
+{
+private:
+	static int getPrecedence(std::string op);
+
+
+public:
+	static void ltrim(std::string& s);
+	static void rtrim(std::string& s);
+	static void trimString(std::string& s);
+	static std::string infixToPostfix(std::vector<spa::Token> tokens);
+};
