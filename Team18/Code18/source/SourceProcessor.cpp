f61cdccbf8c61b416a881d6e4b0f9cd0f19f18de
Winnie Ho Yi Xuan <ho.yixuan6@gmail.com>
2023-01-31 23:10:45 +0800

    Fix identation issues

--- Team18/Code18/source/SourceProcessor.cpp
+++ Team18/Code18/source/SourceProcessor.cpp
@@ -8,17 +8,17 @@
 #include "Validator.h"
 
 void spa::SourceProcessor::processSource(const char* source) {
-	std::stringstream ss;
-	ss.str(source);
-	Tokenizer tokenizer;
-	Stream<Token> tokens = tokenizer.tokenize(ss);
+    std::stringstream ss;
+    ss.str(source);
+    Tokenizer tokenizer;
+    Stream<Token> tokens = tokenizer.tokenize(ss);
 
-	std::cout << "TESTING TOKENIZER" << std::endl;
-	for (int64_t i = 0; i < tokens.remaining(); i++) {
-		std::cout << "Type: " << tokens[i].getType() << ", Value: " << tokens[i].getValue() << std::endl;
-	}
-	std::cout << "END OF TESTING" << std::endl;
+    std::cout << "TESTING TOKENIZER" << std::endl;
+    for (int64_t i = 0; i < tokens.remaining(); i++) {
+        std::cout << "Type: " << tokens[i].getType() << ", Value: " << tokens[i].getValue() << std::endl;
+    }
+    std::cout << "END OF TESTING" << std::endl;
 
-	spa::Validator validator = spa::Validator(tokens);
-	validator.validateProgram();
+    spa::Validator validator = spa::Validator(tokens);
+    validator.validateProgram();
 }
--- Team18/Code18/source/Validator.h
+++ Team18/Code18/source/Validator.h
@@ -4,35 +4,37 @@
 #include "Stream.h"
 #include "PKB.h"
 namespace spa {
-	class Validator {
+    class Validator {
 
-	public:
-		spa::Stream<spa::Token> tokens;
-		int offset = 0;
-		Validator();
-		Validator(spa::Stream<spa::Token> tokens);
-		bool validateProgram();
-		bool setStream(spa::Stream<spa::Token> stream);
-		bool parseStream(spa::Stream<spa::Token> stream);
-		spa::Token& get();
-		spa::Token& next(int offset);
-		spa::Token& next();
-		bool isValidRelExprToken(spa::Token token);
-		bool isValidStmtToken(spa::Token token);
-		bool isValidCondExprToken(spa::Token token);
-		bool isValidTermToken(spa::Token token);
+    public:
+        spa::Stream<spa::Token> tokens;
+        int offset = 0;
+        Validator();
+        Validator(spa::Stream<spa::Token> tokens);
+        bool validateProgram();
+        bool setStream(spa::Stream<spa::Token> stream);
+        bool parseStream(spa::Stream<spa::Token> stream);
+        spa::Token& get();
+        spa::Token& next(int offset);
+        spa::Token& next();
+        bool isValidRelExprToken(spa::Token token);
+        bool isValidStmtToken(spa::Token token);
+        bool isValidCondExprToken(spa::Token token);
+        bool isValidTermToken(spa::Token token);
 
-	private:
-		static bool isValidProgram(spa::Stream<spa::Token> tokens);
-		static bool isValidGrammar();
-		static bool isValidProcedure();
-		static bool isValidCondExpr();
-		static bool isValidAssign();
-		static bool isValidStmt();
-		static bool isValidStmtList();
-		static bool isValidReadPrint();
-		static bool isValidWhile();
-		static bool isValidif();
+    private:
+        static bool isValidProgram(spa::Stream<spa::Token> tokens);
+        static bool isValidGrammar();
+        static bool isValidProcedure();
+        static bool isValidCondExpr();
+        static bool isValidAssign();
+        static bool isValidStmt();
+        static bool isValidStmtList();
+        static bool isValidReadPrint();
+        static bool isValidWhile();
+        static bool isValidif();
 
-	};
+    };
+}
+}
 }
