aa7e632aa021781ade78e159600b63e4d1d6603d
Imranr2 <imranr2@msn.com>
2023-02-03 22:25:24 +0800

    Fix linter issues

--- Team18/Code18/source/SimpleEvaluator.cpp
+++ Team18/Code18/source/SimpleEvaluator.cpp
@@ -8,14 +8,14 @@ spa::QpsResultTable spa::SimpleEvaluator::evaluate(PKBManager& pkbManager) {
   QueryResult result = pkbManager.getEntity(designEntityType);
   QpsResultTable resultTable;
   resultTable.addHeader(selectSynonym);
-  
+
   if (designEntityType == VARIABLE || designEntityType == PROCEDURE || designEntityType == CONSTANT) {
     for (auto& name : result.getNames()) {
       resultTable.addRow({ QpsValue(name) });
     }
     return resultTable;
   }
-  
+
   for (auto& lineNumber : result.getLineNumbers()) {
     resultTable.addRow({ QpsValue(lineNumber) });
   }
