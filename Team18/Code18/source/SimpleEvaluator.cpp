a5b1b2324fec90e61b574183d087f86339f038ba
Imranr2 <imranr2@msn.com>
2023-02-03 22:24:11 +0800

    Add simple evaluator

--- Team18/Code18/source/SimpleEvaluator.cpp
+++ Team18/Code18/source/SimpleEvaluator.cpp
@@ -1,8 +1,23 @@
 #include "SimpleEvaluator.h"
 
-spa::SimpleEvaluator::SimpleEvaluator(DesignEntityType designEntityType) : designEntityType(designEntityType) {
+spa::SimpleEvaluator::SimpleEvaluator(std::string selectSynonym, DesignEntityType designEntityType) :
+  selectSynonym(selectSynonym), designEntityType(designEntityType) {
 }
 
 spa::QpsResultTable spa::SimpleEvaluator::evaluate(PKBManager& pkbManager) {
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  
+  if (designEntityType == VARIABLE || designEntityType == PROCEDURE || designEntityType == CONSTANT) {
+    for (auto& name : result.getNames()) {
+      resultTable.addRow({ QpsValue(name) });
+    }
+    return resultTable;
+  }
+  
+  for (auto& lineNumber : result.getLineNumbers()) {
+    resultTable.addRow({ QpsValue(lineNumber) });
+  }
   return QpsResultTable();
 }
