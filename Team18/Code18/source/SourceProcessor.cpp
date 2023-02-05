3f48c50aa3b6adebe52e3decb00bac2878e81ac0
Imranr2 <imranr2@msn.com>
2023-02-05 11:50:10 +0800

    Fix linter issues

--- Team18/Code18/source/SourceProcessor.cpp
+++ Team18/Code18/source/SourceProcessor.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "Stream.h"
 #include "Tokenizer.h"
@@ -20,12 +21,6 @@ void spa::SourceProcessor::processSource() {
     spa::Validator validator(tokens);
     auto newTokens = validator.convertToken(tokens);
     newTokens.resetStartPosition();
-    //std::cout << "TESTING VALIDATOR" << std::endl;
-    //for (int64_t i = 0; i < newTokens.remaining(); i++) {
-    //    std::cout << "Type: " << newTokens[i].getType() <<
-    //        ", Value: " << newTokens[i].getValue() << std::endl;
-    //}
-    //std::cout << "END OF TESTING" << std::endl;
     validator.validateProgram();
     newTokens.resetStartPosition();
     SpParser parser = SpParser(newTokens);
