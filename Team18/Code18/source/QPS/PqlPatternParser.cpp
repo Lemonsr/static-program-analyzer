4038ced3cefa1caa0e591c0751f08ff8f3cf8da8
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-02-20 21:56:49 +0800

    Add pattern sub parser

--- Team18/Code18/source/QPS/PqlPatternParser.cpp
+++ Team18/Code18/source/QPS/PqlPatternParser.cpp
@@ -8,124 +8,14 @@
 #include "ParsedQuery.h"
 #include "Token.h"
 
-std::optional<spa::Pattern> spa::PqlPatternParser::parseWildcard(
-    Stream<Token>& tokens,
-    ParsedQuery& query
-) {
-  if (!tokens.match({ { TOKEN_UNDERSCORE, "_" } })) {
-    return {};
-  }
-  tokens.seek(1);
-  return { Pattern { ANY } };
-}
-
-std::optional<spa::Pattern> spa::PqlPatternParser::parseExact(
-    Stream<Token>& tokens,
-    ParsedQuery& query
-) {
-  if (!tokens.match({ { TOKEN_DOUBLE_QUOTES, "\"" } })) {
-    return {};
-  }
-  tokens.seek(1);
-  std::vector<Token> patternTokens;
-  while (tokens.remaining() > 0) {
-    if (tokens[0] == Token { TOKEN_DOUBLE_QUOTES, "\"" }) {
-      break;
-    }
-    patternTokens.push_back(tokens[0]);
-    tokens.seek(1);
-  }
-  if (!tokens.match({ { TOKEN_DOUBLE_QUOTES, "\"" } })) {
-    return {};
-  }
-  tokens.seek(1);
-  return { Pattern { EXACT, patternTokens } };
-}
-
-std::optional<spa::Pattern> spa::PqlPatternParser::parsePartial(
-    Stream<Token>& tokens,
-    ParsedQuery& query
-) {
-  bool matchResult = tokens.match({
-    { TOKEN_UNDERSCORE, "_" },
-    { TOKEN_DOUBLE_QUOTES, "\"" }
-  });
-  if (!matchResult) {
-    return {};
-  }
-  tokens.seek(2);
-  std::vector<Token> patternTokens;
-  while (tokens.remaining() > 0) {
-    if (tokens[0] == Token{ TOKEN_DOUBLE_QUOTES, "\"" }) {
-      break;
-    }
-    patternTokens.push_back(tokens[0]);
-    tokens.seek(1);
-  }
-  matchResult = tokens.match({
-    { TOKEN_DOUBLE_QUOTES, "\"" },
-    { TOKEN_UNDERSCORE, "_" }
-  });
-  if (!matchResult) {
-    return {};
-  }
-  tokens.seek(2);
-  return { Pattern { PARTIAL, patternTokens } };
-}
-
-std::optional<spa::Pattern> spa::PqlPatternParser::parsePattern(
-    Stream<Token>& tokens,
-    ParsedQuery& query
-) {
-  std::optional<Pattern> eOptional = parseExact(tokens, query);
-  if (eOptional) {
-    return eOptional;
-  }
-  std::optional<Pattern> pOptional = parsePartial(tokens, query);
-  if (pOptional) {
-    return pOptional;
-  }
-  std::optional<Pattern> wOptional = parseWildcard(tokens, query);
-  if (wOptional) {
-    return wOptional;
-  }
-  return {};
-}
-
 spa::PqlParseStatus spa::PqlPatternParser::parse(Stream<Token>& tokens,
                                                  ParsedQuery& query) {
   bool matchResult = tokens.match({
-    { spa::TOKEN_NAME, "pattern"},
-    { spa::TOKEN_NAME, ""},
-    { spa::TOKEN_OPEN_BRACKET, "("},
+    { spa::TOKEN_NAME, "pattern"}
   });
   if (!matchResult) {
     return PQL_PARSE_MISMATCH;
   }
-  std::string synAssign = tokens[1].getValue();
-  std::optional<DesignEntityType> synOpt = query.getType(synAssign);
-  if (!synOpt || synOpt.value() != ASSIGN) {
-    return PQL_PARSE_ERROR;
-  }
-  PqlArgument assignSynonym(SYNONYM, synAssign, { ASSIGN });
-  tokens.seek(3);
-  std::optional<PqlArgument> firstOpt = argParser.parse(tokens, query);
-  if (!firstOpt) {
-    return PQL_PARSE_ERROR;
-  }
-  if (!tokens.match({{ spa::TOKEN_COMMA, ","}})) {
-    return PQL_PARSE_ERROR;
-  }
   tokens.seek(1);
-  std::optional<Pattern> patternOpt = parsePattern(tokens, query);
-  if (!patternOpt) {
-    return PQL_PARSE_ERROR;
-  }
-  if (!tokens.match({ { spa::TOKEN_CLOSE_BRACKET, ")"} })) {
-    return PQL_PARSE_ERROR;
-  }
-  tokens.seek(1);
-  query.setPatternClause(
-    { assignSynonym, firstOpt.value(), patternOpt.value() });
-  return PQL_PARSE_SUCCESS;
+  return subParser.parse(tokens, query);
 }
---
+++ Team18/Code18/source/QPS/PqlPatternSubParser.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <optional>
+
+#include "PqlArgumentParser.h"
+#include "PqlParser.h"
+#include "ParsedQuery.h"
+
+namespace spa {
+  class PqlPatternSubParser : public PqlParser {
+  private:
+    PqlArgumentParser argParser;
+    std::optional<Pattern> parseWildcard(Stream<Token>& tokens,
+      ParsedQuery& query);
+    std::optional<Pattern> parseExact(Stream<Token>& tokens,
+      ParsedQuery& query);
+    std::optional<Pattern> parsePartial(Stream<Token>& tokens,
+      ParsedQuery& query);
+    std::optional<Pattern> parsePattern(Stream<Token>& tokens,
+      ParsedQuery& query);
+    std::optional<PqlArgument> parseDesignEntity(Token& token, ParsedQuery& query);
+  public:
+    PqlParseStatus parse(Stream<Token>& tokens, ParsedQuery& query);
+  };
+}  // namespace spa
