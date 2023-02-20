3edd245ef48d201bc7cacfbc9d64ce9b8a224a5f
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-02-20 22:08:25 +0800

    Add tests for pattern parsers

--- Team18/Code18/UnitTesting/TestPqlPatternSubParser.cpp
+++ Team18/Code18/UnitTesting/TestPqlPatternSubParser.cpp
@@ -8,18 +8,17 @@
 #include "DesignEntity.h"
 #include "ParsedQuery.h"
 #include "PqlParser.h"
-#include "PqlPatternParser.h"
+#include "PqlPatternSubParser.h"
 #include "Stream.h"
 #include "Token.h"
 
 using Microsoft::VisualStudio::CppUnitTestFramework::Assert;
 
 namespace UnitTesting {
-  TEST_CLASS(TestPqlPatternParser) {
+  TEST_CLASS(TestPqlPatternSubParser) {
 public:
-  TEST_METHOD(TestCorrectSyntax) {
+  TEST_METHOD(TestWildcard) {
     spa::Stream<spa::Token> tokens;
-    tokens.pushBack({ spa::TOKEN_NAME, "pattern" });
     tokens.pushBack({ spa::TOKEN_NAME, "a" });
     tokens.pushBack({ spa::TOKEN_OPEN_BRACKET, "(" });
     tokens.pushBack({ spa::TOKEN_NAME, "v" });
@@ -29,7 +28,7 @@ public:
     spa::ParsedQuery query;
     query.addDeclaration("a", spa::ASSIGN);
     query.addDeclaration("v", spa::VARIABLE);
-    spa::PqlPatternParser parser;
+    spa::PqlPatternSubParser parser;
     spa::PqlParseStatus status = parser.parse(tokens, query);
     Assert::IsTrue(status == spa::PQL_PARSE_SUCCESS);
     std::optional<spa::PatternClause> opt = query.getPatternClause();
@@ -43,31 +42,46 @@ public:
     Assert::AreEqual(tokens.remaining(), int64_t(0));
   }
 
-  TEST_METHOD(TestMismatchSyntax) {
+  TEST_METHOD(TestPartial) {
     spa::Stream<spa::Token> tokens;
-    tokens.pushBack({ spa::TOKEN_NAME, "such" });
-    tokens.pushBack({ spa::TOKEN_NAME, "that" });
-    tokens.pushBack({ spa::TOKEN_NAME, "Modifies" });
-    tokens.pushBack({ spa::TOKEN_OPEN_BRACKET, "(" });
     tokens.pushBack({ spa::TOKEN_NAME, "a" });
-    tokens.pushBack({ spa::TOKEN_COMMA, "," });
+    tokens.pushBack({ spa::TOKEN_OPEN_BRACKET, "(" });
     tokens.pushBack({ spa::TOKEN_NAME, "v" });
+    tokens.pushBack({ spa::TOKEN_COMMA, "," });
+    tokens.pushBack({ spa::TOKEN_UNDERSCORE, "_" });
+    tokens.pushBack({ spa::TOKEN_DOUBLE_QUOTES, "\"" });
+    tokens.pushBack({ spa::TOKEN_NAME, "x" });
+    tokens.pushBack({ spa::TOKEN_PLUS, "+" });
+    tokens.pushBack({ spa::TOKEN_NAME, "y" });
+    tokens.pushBack({ spa::TOKEN_DOUBLE_QUOTES, "\"" });
+    tokens.pushBack({ spa::TOKEN_UNDERSCORE, "_" });
     tokens.pushBack({ spa::TOKEN_CLOSE_BRACKET, ")" });
     spa::ParsedQuery query;
     query.addDeclaration("a", spa::ASSIGN);
     query.addDeclaration("v", spa::VARIABLE);
-    spa::PqlPatternParser parser;
+    spa::PqlPatternSubParser parser;
     spa::PqlParseStatus status = parser.parse(tokens, query);
-    Assert::IsTrue(status == spa::PQL_PARSE_MISMATCH);
+    Assert::IsTrue(status == spa::PQL_PARSE_SUCCESS);
+    std::optional<spa::PatternClause> opt = query.getPatternClause();
+    Assert::IsTrue(opt.has_value());
+    spa::PatternClause& clause = opt.value();
+    spa::PatternClause compare(
+      { spa::SYNONYM, "a", spa::ASSIGN },
+      spa::PqlArgument(spa::SYNONYM, "v", { spa::VARIABLE }),
+      spa::Pattern(spa::PARTIAL, {
+        { spa::TOKEN_NAME, "x" },
+        { spa::TOKEN_PLUS, "+" },
+        { spa::TOKEN_NAME, "y" }
+        }));
+    Assert::IsTrue(clause == compare);
+    Assert::AreEqual(tokens.remaining(), int64_t(0));
   }
 
-  TEST_METHOD(TestErrorSyntax) {
+  TEST_METHOD(TestExact) {
     spa::Stream<spa::Token> tokens;
-    tokens.pushBack({ spa::TOKEN_NAME, "pattern" });
     tokens.pushBack({ spa::TOKEN_NAME, "a" });
     tokens.pushBack({ spa::TOKEN_OPEN_BRACKET, "(" });
     tokens.pushBack({ spa::TOKEN_NAME, "v" });
-    tokens.pushBack({ spa::TOKEN_COMMA, "," });
     tokens.pushBack({ spa::TOKEN_COMMA, "," });
     tokens.pushBack({ spa::TOKEN_DOUBLE_QUOTES, "\"" });
     tokens.pushBack({ spa::TOKEN_NAME, "x" });
@@ -78,9 +92,22 @@ public:
     spa::ParsedQuery query;
     query.addDeclaration("a", spa::ASSIGN);
     query.addDeclaration("v", spa::VARIABLE);
-    spa::PqlPatternParser parser;
+    spa::PqlPatternSubParser parser;
     spa::PqlParseStatus status = parser.parse(tokens, query);
-    Assert::IsTrue(status == spa::PQL_PARSE_ERROR);
+    Assert::IsTrue(status == spa::PQL_PARSE_SUCCESS);
+    std::optional<spa::PatternClause> opt = query.getPatternClause();
+    Assert::IsTrue(opt.has_value());
+    spa::PatternClause& clause = opt.value();
+    spa::PatternClause compare(
+      { spa::SYNONYM, "a", spa::ASSIGN },
+      spa::PqlArgument(spa::SYNONYM, "v", { spa::VARIABLE }),
+      spa::Pattern(spa::EXACT, {
+        { spa::TOKEN_NAME, "x" },
+        { spa::TOKEN_PLUS, "+" },
+        { spa::TOKEN_NAME, "y" }
+        }));
+    Assert::IsTrue(clause == compare);
+    Assert::AreEqual(tokens.remaining(), int64_t(0));
   }
   };
 }  // namespace UnitTesting
