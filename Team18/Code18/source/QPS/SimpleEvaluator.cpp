31461a2b886520638b5c6a4e073a8e95e5222b3e
Imranr2 <imranr2@msn.com>
2023-02-23 00:17:16 +0800

    Change SimpleEvaluator to return all attribute types

--- Team18/Code18/source/QPS/SimpleEvaluator.cpp
+++ Team18/Code18/source/QPS/SimpleEvaluator.cpp
@@ -1,23 +1,220 @@
 #include "SimpleEvaluator.h"
+#include "PqlAttributes.h"
+
+const std::unordered_map<spa::DesignEntityType,
+  std::function<spa::QpsResultTable(spa::SimpleEvaluator&, spa::PKBManager&)>> designEntityEvaluateFunctionMap {
+    {{spa::DesignEntityType::PROCEDURE}, &spa::SimpleEvaluator::evaluateProcedure},
+    {{spa::DesignEntityType::STMT}, &spa::SimpleEvaluator::evaluateStmt},
+    {{spa::DesignEntityType::READ}, &spa::SimpleEvaluator::evaluateRead},
+    {{spa::DesignEntityType::PRINT}, &spa::SimpleEvaluator::evaluatePrint},
+    {{spa::DesignEntityType::ASSIGN}, &spa::SimpleEvaluator::evaluateAssign},
+    {{spa::DesignEntityType::CALL}, &spa::SimpleEvaluator::evaluateCall},
+    {{spa::DesignEntityType::WHILE}, &spa::SimpleEvaluator::evaluateWhile},
+    {{spa::DesignEntityType::IF}, &spa::SimpleEvaluator::evaluateIf},
+    {{spa::DesignEntityType::VARIABLE}, &spa::SimpleEvaluator::evaluateVariable},
+    {{spa::DesignEntityType::CONSTANT}, &spa::SimpleEvaluator::evaluateConstant}
+};
 
 spa::SimpleEvaluator::SimpleEvaluator(std::string selectSynonym, DesignEntityType designEntityType) :
   selectSynonym(selectSynonym), designEntityType(designEntityType) {
 }
 
 spa::QpsResultTable spa::SimpleEvaluator::evaluate(PKBManager& pkbManager) {
-  QueryResult result = pkbManager.getEntity(designEntityType);
+  switch (designEntityType) {
+  case PROCEDURE: {
+    return evaluateProcedure(pkbManager);
+  }
+  case STMT: {
+    return evaluateStmt(pkbManager);
+  }
+  case READ: {
+    return evaluateRead(pkbManager);
+  }
+  case PRINT: {
+    return evaluatePrint(pkbManager);
+  }
+  case ASSIGN: {
+    return evaluateAssign(pkbManager);
+  }
+  case CALL: {
+    return evaluateCall(pkbManager);
+  }
+  case WHILE: {
+    return evaluateWhile(pkbManager);
+  }
+  case IF: {
+    return evaluateIf(pkbManager);
+  }
+  case VARIABLE: {
+    return evaluateVariable(pkbManager);
+  }
+  case CONSTANT: {
+    return evaluateConstant(pkbManager);
+  }
+  default: {
+    return {};
+  }
+  }
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateProcedure(PKBManager& pkbManager) {
   QpsResultTable resultTable;
   resultTable.addHeader(selectSynonym);
-
-  if (designEntityType == VARIABLE || designEntityType == PROCEDURE || designEntityType == CONSTANT) {
-    for (auto& name : result.getNames()) {
-      resultTable.addRow({ QpsValue(name) });
-    }
-    return resultTable;
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
   }
 
-  for (auto& lineNumber : result.getLineNumbers()) {
-    resultTable.addRow({ QpsValue(lineNumber) });
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& procName : result.getNames()) {
+    resultTable.addRow({ QpsValue(procName), QpsValue(procName) });
   }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateStmt(PKBManager& pkbManager) {
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& stmtNum : result.getLineNumbers()) {
+    resultTable.addRow({ QpsValue(stmtNum), QpsValue(stmtNum) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateRead(PKBManager& pkbManager) {
+  PKBQueryArg readSynonymArg(PqlArgument(ArgumentType::SYNONYM, "re", designEntityType));
+  PKBQueryArg varSynonymArg(PqlArgument(ArgumentType::SYNONYM, "v", DesignEntityType::VARIABLE));
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getRelationship(RelationshipType::MODIFIES, readSynonymArg, varSynonymArg);
+  for (auto& pair : result.getLineNumberVariablePairs()) {
+    int lineNumber = pair.first;
+    std::string varName = pair.second;
+    resultTable.addRow({ QpsValue(lineNumber), QpsValue(lineNumber), QpsValue(varName) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluatePrint(PKBManager& pkbManager) {
+  PKBQueryArg readSynonymArg(PqlArgument(ArgumentType::SYNONYM, "pr", designEntityType));
+  PKBQueryArg varSynonymArg(PqlArgument(ArgumentType::SYNONYM, "v", DesignEntityType::VARIABLE));
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getRelationship(RelationshipType::MODIFIES, readSynonymArg, varSynonymArg);
+  for (auto& pair : result.getLineNumberVariablePairs()) {
+    int lineNumber = pair.first;
+    std::string varName = pair.second;
+    resultTable.addRow({ QpsValue(lineNumber), QpsValue(lineNumber), QpsValue(varName) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateAssign(PKBManager& pkbManager) {
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& assignNum : result.getLineNumbers()) {
+    resultTable.addRow({ QpsValue(assignNum), QpsValue(assignNum) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateCall(PKBManager& pkbManager) {
+  PKBQueryArg readSynonymArg(PqlArgument(ArgumentType::SYNONYM, "c", designEntityType));
+  PKBQueryArg varSynonymArg(PqlArgument(ArgumentType::SYNONYM, "v", DesignEntityType::VARIABLE));
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getRelationship(RelationshipType::MODIFIES, readSynonymArg, varSynonymArg);
+  for (auto& pair : result.getLineNumberVariablePairs()) {
+    int lineNumber = pair.first;
+    std::string varName = pair.second;
+    resultTable.addRow({ QpsValue(lineNumber), QpsValue(lineNumber), QpsValue(varName) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateWhile(PKBManager& pkbManager) {
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& assignNum : result.getLineNumbers()) {
+    resultTable.addRow({ QpsValue(assignNum), QpsValue(assignNum) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateIf(PKBManager& pkbManager) {
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& assignNum : result.getLineNumbers()) {
+    resultTable.addRow({ QpsValue(assignNum), QpsValue(assignNum) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateVariable(PKBManager& pkbManager) {
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& varName : result.getNames()) {
+    resultTable.addRow({ QpsValue(varName), QpsValue(varName) });
+  }
+
+  return resultTable;
+}
+
+spa::QpsResultTable spa::SimpleEvaluator::evaluateConstant(PKBManager& pkbManager) {
+  QpsResultTable resultTable;
+  resultTable.addHeader(selectSynonym);
+  for (auto& attr : pqlAttributesMap[designEntityType]) {
+    resultTable.addHeader(selectSynonym.append(".").append(attr));
+  }
+
+  QueryResult result = pkbManager.getEntity(designEntityType);
+  for (auto& varName : result.getNames()) {
+    resultTable.addRow({ QpsValue(varName), QpsValue(varName) });
+  }
+
   return resultTable;
 }
--- Team18/Code18/source/QPS/SimpleEvaluator.h
+++ Team18/Code18/source/QPS/SimpleEvaluator.h
@@ -5,14 +5,28 @@
 #include "QpsResultTable.h"
 
 #include <string>
+#include <functional>
+#include <unordered_map>
 
 namespace spa {
 class SimpleEvaluator : public QpsEvaluator {
  private:
+  std::unordered_map<DesignEntityType, std::function<QpsResultTable(SimpleEvaluator, PKBManager)>> designEntityEvaluateFunctionMap;
   std::string selectSynonym;
   DesignEntityType designEntityType;
+
  public:
   explicit SimpleEvaluator(std::string selectSynonym, DesignEntityType designEntityType);
   QpsResultTable evaluate(PKBManager& pkbManager);
+  QpsResultTable evaluateProcedure(PKBManager& pkbManager);
+  QpsResultTable evaluateStmt(PKBManager& pkbManager);
+  QpsResultTable evaluateRead(PKBManager& pkbManager);
+  QpsResultTable evaluatePrint(PKBManager& pkbManager);
+  QpsResultTable evaluateAssign(PKBManager& pkbManager);
+  QpsResultTable evaluateCall(PKBManager& pkbManager);
+  QpsResultTable evaluateWhile(PKBManager& pkbManager);
+  QpsResultTable evaluateIf(PKBManager& pkbManager);
+  QpsResultTable evaluateVariable(PKBManager& pkbManager);
+  QpsResultTable evaluateConstant(PKBManager& pkbManager);
 };
 }  // namespace spa
