d2d67f76b997898d802a17702386cf2970ac095b
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-03-05 13:34:27 +0800

    Create tests for qps

---
+++ Team18/Code18/IntegrationTesting/TestQps.cpp
@@ -0,0 +1,42 @@
+#include <memory>
+#include <string>
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "QPS.h"
+#include "PKB.h"
+#include "PqlParser.h"
+
+using Microsoft::VisualStudio::CppUnitTestFramework::Assert;
+
+namespace IntegrationTesting {
+TEST_CLASS(TestQPS) {
+ public:
+  TEST_METHOD(TestValidQuery) {
+    spa::QPS qps;
+    std::unique_ptr<spa::PKBManager> pkbManager = std::make_unique<spa::PKB>();
+    std::string query = "variable v; Select v";
+    spa::QpsResult result = qps.evaluate(query, *pkbManager);
+    Assert::IsFalse(result.getErrorMessage().has_value());
+  }
+
+  TEST_METHOD(TestSyntacticallyInvalidQuery) {
+    spa::QPS qps;
+    std::unique_ptr<spa::PKBManager> pkbManager = std::make_unique<spa::PKB>();
+    std::string query = "variable v; Select v;";
+    spa::QpsResult result = qps.evaluate(query, *pkbManager);
+    Assert::IsTrue(result.getErrorMessage().has_value());
+    Assert::AreEqual(result.getErrorMessage().value(), std::string("SyntaxError"));
+  }
+
+  TEST_METHOD(TestSemanticallyInvalidQuery) {
+    spa::QPS qps;
+    std::unique_ptr<spa::PKBManager> pkbManager = std::make_unique<spa::PKB>();
+    std::string query = "variable v; Select v such that Modifies(_, v)";
+    spa::QpsResult result = qps.evaluate(query, *pkbManager);
+    Assert::IsTrue(result.getErrorMessage().has_value());
+    Assert::AreEqual(result.getErrorMessage().value(), std::string("SemanticError"));
+  }
+};
+}  // namespace IntegrationTesting
