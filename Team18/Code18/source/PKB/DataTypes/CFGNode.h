73b88fa43b962fcf2b4b91fb767e9f089f9c5c52
Winnie Ho Yi Xuan <ho.yixuan6@gmail.com>
2023-03-05 17:18:52 +0800

    Create CFGNode class

--- Team18/Code18/source/PKB/DataTypes/CFGNode.h
+++ Team18/Code18/source/PKB/DataTypes/CFGNode.h
@@ -1,7 +1,28 @@
 #pragma once
+#include <string>
+#include <unordered_set>
 
 namespace spa {
 class CFGNode {
  public:
-};
+    CFGNode(int lineNumber);
+    int getLineNumber() const;
+    void linkTo(CFGNode* node);
+    void addModifiedVariable(std::string variable);
+    std::unordered_set<std::string> getModifiedVariables() const;
+    std::unordered_set<CFGNode*> getIncomingEdges() const;
+    std::unordered_set<CFGNode*> getOutgoingEdges() const;
+    void addInEdges(std::unordered_set<CFGNode*> edges);
+    void addOutEdges(std::unordered_set<CFGNode*> edges);
+    void removeIncomingNode(CFGNode* node);
+    void removeOutgoingNode(CFGNode* node);
+    void removeNodeFromGraph();
+
+private:
+    int lineNumber;
+    std::unordered_set<std::string> modifiedVariables;
+    std::unordered_set<CFGNode*> incomingEdges;
+    std::unordered_set<CFGNode*> outgoingEdges;
+};;
 }  // namespace spa
+
