582ef9f85e3fb8e9e9cf796e73d367978044932e
Imranr2 <imranr2@msn.com>
2023-03-06 20:09:52 +0800

    Implement query optimizer to create groups for evaluation

---
+++ Team18/Code18/source/QPS/Evaluators/QueryOptimizer.cpp
@@ -0,0 +1,76 @@
+#include "QueryOptimizer.h"
+
+#include <unordered_set>
+#include <queue>
+
+spa::ConnectedSynonymClauseGroup spa::QueryOptimizer::groupConnectedComponents(Clause* clause) {
+  std::queue<Clause*> connectedClauses;
+  ConnectedSynonymClauseGroup connectedSynonymClauseGroup;
+  connectedClauses.push(clause);
+
+  while (!connectedClauses.empty()) {
+    Clause* clause = connectedClauses.front();
+    connectedSynonymClauseGroup.addClause(*clause);
+    connectedClauses.pop();
+    visitedClauses.insert(clause);
+
+    for (auto& synonym : clause->getArgValues()) {
+      for (auto& nei : synonymClauseMap[synonym]) {
+        if (visitedClauses.find(nei) != visitedClauses.end()) {
+          continue;
+        }
+        connectedClauses.push(nei);
+      }
+    }
+  }
+
+  return connectedSynonymClauseGroup;
+}
+
+std::unordered_map<std::string, spa::ConnectedSynonymClauseGroup> spa::QueryOptimizer::getSynonymGroupMap() {
+  return std::unordered_map<std::string, ConnectedSynonymClauseGroup>();
+}
+
+std::pair<spa::NoSynonymClauseGroup,
+  std::vector<spa::ConnectedSynonymClauseGroup>> spa::QueryOptimizer::getGroups(ParsedQuery& parsedQuery) {
+  initialize(parsedQuery);
+  if (clausesWithSynonyms.empty()) {
+    return { noSynonymClauseGroup, connectedSynonymClauseGroups };
+  }
+
+  for (auto& clause : clausesWithSynonyms) {
+    if (visitedClauses.find(clause) != visitedClauses.end()) {
+      continue;
+    }
+    connectedSynonymClauseGroups.push_back(groupConnectedComponents(clause));
+  }
+
+  return { noSynonymClauseGroup, connectedSynonymClauseGroups };
+}
+
+void spa::QueryOptimizer::initialize(ParsedQuery& parsedQuery) {
+  for (auto& clause : parsedQuery.getSuchThatClauses()) {
+    if (clause.getFirstArgType() != SYNONYM && clause.getSecondArgType() != SYNONYM) {
+      noSynonymClauseGroup.addClause(clause);
+      continue;
+    }
+
+    clausesWithSynonyms.push_back(&clause);
+    if (clause.getFirstArgType() == spa::SYNONYM) {
+      synonymClauseMap[clause.getFirstArgValue()].push_back(&clause);
+    }
+
+    if (clause.getSecondArgType() == spa::SYNONYM) {
+      synonymClauseMap[clause.getSecondArgValue()].push_back(&clause);
+    }
+  }
+
+  for (auto& clause : parsedQuery.getPatternClauses()) {
+    clausesWithSynonyms.push_back(&clause);
+    synonymClauseMap[clause.getSynonymValue()].push_back(&clause);
+
+    if (clause.getFirstArgType() == spa::SYNONYM) {
+      synonymClauseMap[clause.getFirstArgValue()].push_back(&clause);
+    }
+  }
+}
---
+++ Team18/Code18/source/QPS/Evaluators/QueryOptimizer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "Clause.h"
+#include "NoSynonymClauseGroup.h"
+#include "ConnectedSynonymClauseGroup.h"
+#include "ParsedQuery.h"
+
+namespace spa {
+class QueryOptimizer {
+ private:
+  std::unordered_map<std::string, std::vector<Clause*>> synonymClauseMap;
+  NoSynonymClauseGroup noSynonymClauseGroup;
+  std::vector<ConnectedSynonymClauseGroup> connectedSynonymClauseGroups;
+  std::vector<Clause*> clausesWithSynonyms;
+  std::unordered_set<Clause*> visitedClauses;
+
+  void initialize(ParsedQuery& parsedQuery);
+  ConnectedSynonymClauseGroup groupConnectedComponents(Clause* clause);
+  std::unordered_map<std::string, ConnectedSynonymClauseGroup> getSynonymGroupMap();
+ public:
+  std::pair<NoSynonymClauseGroup, std::vector<ConnectedSynonymClauseGroup>> getGroups(ParsedQuery& parsedQuery);
+};
+}  // namespace spa
