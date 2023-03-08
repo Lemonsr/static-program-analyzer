bd49e6027c485efe7e5820a4a20ddd170bf11a89
Imranr2 <imranr2@msn.com>
2023-03-08 10:24:43 +0800

    Fix PR comments

--- Team18/Code18/source/QPS/Evaluators/QueryOptimizer.cpp
+++ Team18/Code18/source/QPS/Evaluators/QueryOptimizer.cpp
@@ -46,47 +46,35 @@ spa::ClauseGroups spa::QueryOptimizer::getGroups(ParsedQuery& parsedQuery) {
 
 void spa::QueryOptimizer::initialize(ParsedQuery& parsedQuery) {
   for (auto& clause : parsedQuery.getSuchThatClauses()) {
-    if (clause.getFirstArgType() != SYNONYM && clause.getSecondArgType() != SYNONYM) {
+    std::vector<std::string> synonyms = clause.getSynonyms();
+    if (synonyms.empty()) {
       noSynonymClauseGroup.addClause(clause);
       continue;
     }
 
     clausesWithSynonyms.push_back(&clause);
-    if (clause.getFirstArgType() == spa::SYNONYM) {
-      synonymClauseMap[clause.getFirstArgValue()].push_back(&clause);
-    }
-
-    if (clause.getSecondArgType() == spa::SYNONYM) {
-      synonymClauseMap[clause.getSecondArgValue()].push_back(&clause);
+    for (auto& synonym : synonyms) {
+      synonymClauseMap[synonym].push_back(&clause);
     }
   }
 
   for (auto& clause : parsedQuery.getPatternClauses()) {
     clausesWithSynonyms.push_back(&clause);
-    synonymClauseMap[clause.getSynonymValue()].push_back(&clause);
-
-    if (clause.getFirstArgType() == spa::SYNONYM) {
-      synonymClauseMap[clause.getFirstArgValue()].push_back(&clause);
+    std::vector<std::string> synonyms = clause.getSynonyms();
+    for (auto& synonym : synonyms) {
+      synonymClauseMap[synonym].push_back(&clause);
     }
   }
 
   for (auto& clause : parsedQuery.getWithClauses()) {
-    if (clause.getFirstArgType() == WithArgumentType::WITH_VALUE &&
-      clause.getSecondArgType() == WithArgumentType::WITH_VALUE) {
-      noSynonymClauseGroup.addClause(clause);
-      continue;
-    }
-
-    if (clause.getFirstArgType() == WithArgumentType::WITH_ATTRIBUTE &&
-      clause.getSecondArgType() == WithArgumentType::WITH_ATTRIBUTE) {
-      withAttrAttrClauseGroup.addClause(clause);
-      continue;
-    }
-
-    clausesWithSynonyms.push_back(&clause);
     std::vector<std::string> synonyms = clause.getSynonyms();
-    for (auto& synonym : synonyms) {
-      synonymClauseMap[synonym].push_back(&clause);
+    if (synonyms.size() == 0) {
+      noSynonymClauseGroup.addClause(clause);
+    } else if (synonyms.size() == 2) {
+      withAttrAttrClauseGroup.addClause(clause);
+    } else {
+      clausesWithSynonyms.push_back(&clause);
+      synonymClauseMap[synonyms[0]].push_back(&clause);
     }
   }
 }
