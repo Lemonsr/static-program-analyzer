f34dc3c313f372e96d1665634a9083eec6bb2079
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-03-11 19:23:26 +0800

    Modify inner join in qps result table

--- Team18/Code18/source/QPS/Evaluators/QpsResultTable.h
+++ Team18/Code18/source/QPS/Evaluators/QpsResultTable.h
@@ -14,21 +14,21 @@ namespace spa {
 
 using QpsResultRow = std::vector<QpsValue>;
 
-using QpsValueRowsMap = std::unordered_map<QpsValue, std::vector<QpsResultRow*>,
-                                           QpsValueHash, QpsValueEquality>;
-
-using QpsResultRowPair = std::pair<QpsResultRow*, QpsResultRow*>;
-
-struct QpsResultRowPairHash {
-  size_t operator()(const QpsResultRowPair& p) const;
+struct QpsFilteredRow {
+  std::vector<int>& selectedColumns;
+  QpsResultRow& row;
 };
 
-struct QpsResultRowPairEquality {
-  bool operator()(const QpsResultRowPair& p1, const QpsResultRowPair& p2) const;
+struct QpsFilteredRowHash {
+  size_t operator()(const QpsFilteredRow& r) const;
 };
 
-using QpsResultRowPairMatchMap = std::unordered_map<QpsResultRowPair, int,
-                                                    QpsResultRowPairHash, QpsResultRowPairEquality>;
+struct QpsFilteredRowEquality {
+  bool operator()(const QpsFilteredRow& first, const QpsFilteredRow& second) const;
+};
+
+using QpsRowMap = std::unordered_map<QpsFilteredRow, std::vector<QpsResultRow*>,
+                                     QpsFilteredRowHash, QpsFilteredRowEquality>;
 
 using QpsValueSet = std::unordered_set<QpsValue, QpsValueHash, QpsValueEquality>;
 
@@ -37,14 +37,11 @@ class QpsResultTable {
   std::unordered_map<std::string, std::vector<int>> headerIndexMap;
   std::vector<std::string> headers;
   std::vector<QpsResultRow> rows;
-  std::vector<std::pair<int, int>> getCommonColumnIndices(QpsResultTable& table,
-                                                          QpsResultTable& other);
+  std::pair<std::vector<int>, std::vector<int>> getCommonColumnIndices(QpsResultTable& table,
+                                                                       QpsResultTable& other);
   QpsResultRow concatenateRow(QpsResultRow& row, QpsResultRow& other);
   void cartesianProduct(QpsResultTable& result, QpsResultTable& other);
-  QpsValueRowsMap getValueRowsMap(QpsResultTable& table, int index);
-  void fillQpsResultRowPairMatchMap(QpsResultRowPairMatchMap& result,
-                                    QpsValueRowsMap& cMap,
-                                    QpsValueRowsMap& oMap);
+  void fillQpsRowMap(QpsRowMap& m, QpsResultTable& table, std::vector<int>& selectedColumns);
  public:
   void addHeader(const std::string& header);
   void addHeader(PqlArgument header);
