faa187dc6257d5a356878846df5a3de1f77ca6f1
Liaw Xin Yan <e0580070@u.nus.edu>
2023-03-23 20:10:20 +0800

    Add affectsStar table tests

--- Team18/Code18/UnitTesting/TestPKBAffectsStar.cpp
+++ Team18/Code18/UnitTesting/TestPKBAffectsStar.cpp
@@ -26,5 +26,253 @@ namespace UnitTesting {
       {3, spa::StatementType::ASSIGN},
       {4, spa::StatementType::ASSIGN}
     };
+
+  TEST_METHOD(TestGetAffectsStarLineLine) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "1", {}));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "3", {}));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarLineLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsTrue(queryResult.getIsTrue());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "4", {}));
+    queryResult = relationshipStorage.getAffectsStarLineLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsTrue(queryResult.getIsTrue());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "10", {}));
+    queryResult = relationshipStorage.getAffectsStarLineLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsFalse(queryResult.getIsTrue());
+  }
+
+  TEST_METHOD(TestGetAffectsStarLineStatement) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+    std::vector<std::pair<int, int>> expected = { {1, 3}, {1, 4} };
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "1", {}));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s",
+                                                                   spa::DesignEntityType::STMT));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarLineStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a", spa::DesignEntityType::ASSIGN));
+    queryResult = relationshipStorage.getAffectsStarLineStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    expected = { {2, 3}, {2, 4} };
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "2", {}));
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a",
+                                                  spa::DesignEntityType::ASSIGN));
+    queryResult = relationshipStorage.getAffectsStarLineStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "w", spa::DesignEntityType::WHILE));
+    queryResult = relationshipStorage.getAffectsStarLineStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(queryResult.getLineNumberLineNumberPairs().empty());
+  }
+
+  TEST_METHOD(TestGetAffectsStarStatementLine) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+    std::vector<std::pair<int, int>> expected = { {1, 3}, {2, 3} };
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a",
+                                                                  spa::DesignEntityType::ASSIGN));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "3", {}));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarStatementLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s", spa::DesignEntityType::STMT));
+    queryResult = relationshipStorage.getAffectsStarStatementLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "if", spa::DesignEntityType::IF));
+    queryResult = relationshipStorage.getAffectsStarStatementLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(queryResult.getLineNumberLineNumberPairs().empty());
+  }
+
+  TEST_METHOD(TestGetAffectsStarLineUnderscore) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "2", {}));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::WILDCARD, "_", {}));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarLineUnderscore(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsTrue(queryResult.getIsTrue());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "9", {}));
+    queryResult = relationshipStorage.getAffectsStarLineUnderscore(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsFalse(queryResult.getIsTrue());
+  }
+
+  TEST_METHOD(TestGetAffectsStarUnderscoreLine) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::WILDCARD, "_", {}));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "3", {}));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarUnderscoreLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsTrue(queryResult.getIsTrue());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::LINE_NO, "9", {}));
+    queryResult = relationshipStorage.getAffectsStarUnderscoreLine(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsFalse(queryResult.getIsTrue());
+  }
+
+  TEST_METHOD(TestGetAffectsStarStatementStatement) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+    std::vector<std::pair<int, int>> expected = { {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4} };
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s",
+                                                                  spa::DesignEntityType::STMT));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a",
+                                                                   spa::DesignEntityType::ASSIGN));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarStatementStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "if", spa::DesignEntityType::IF));
+    queryResult = relationshipStorage.getAffectsStarStatementStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(queryResult.getLineNumberLineNumberPairs().empty());
+  }
+
+  TEST_METHOD(TestGetAffectsStarStatementUnderscore) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+    std::vector<std::pair<int, int>> expected = { {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4} };
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a",
+                                                                  spa::DesignEntityType::ASSIGN));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::WILDCARD, "_", {}));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarStatementUnderscore(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s", spa::DesignEntityType::STMT));
+    queryResult = relationshipStorage.getAffectsStarStatementUnderscore(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "p", spa::DesignEntityType::PRINT));
+    queryResult = relationshipStorage.getAffectsStarStatementUnderscore(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(queryResult.getLineNumberLineNumberPairs().empty());
+  }
+
+  TEST_METHOD(TestGetAffectsStarUnderscoreStatement) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+    std::vector<std::pair<int, int>> expected = { {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4} };
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::WILDCARD, "_", {}));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a",
+                                                                   spa::DesignEntityType::ASSIGN));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarUnderscoreStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s", spa::DesignEntityType::STMT));
+    queryResult = relationshipStorage.getAffectsStarUnderscoreStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "re", spa::DesignEntityType::READ));
+    queryResult = relationshipStorage.getAffectsStarUnderscoreStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(queryResult.getLineNumberLineNumberPairs().empty());
+  }
+
+  TEST_METHOD(TestGetAffectsStarUnderscoreUnderscore) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::WILDCARD, "_", {}));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::WILDCARD, "_", {}));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarUnderscoreUnderscore(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::BOOL);
+    Assert::IsTrue(queryResult.getIsTrue());
+
+    relationshipStorage.setAffectsStarTable({});
+    queryResult = relationshipStorage.getAffectsStarUnderscoreUnderscore(firstArg, secondArg);
+    Assert::IsFalse(queryResult.getIsTrue());
+  }
+
+  TEST_METHOD(TestGetAffectsStarGeneralStatementStatement) {
+    spa::RelationshipStorage relationshipStorage;
+    relationshipStorage.setAffectsStarTable(affectsStarTable);
+    relationshipStorage.setStatementTypeTable(statementTypeTable);
+    std::vector<std::pair<int, int>> expected = { {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4} };
+
+    spa::PKBQueryArg firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "a",
+                                                                  spa::DesignEntityType::ASSIGN));
+    spa::PKBQueryArg secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s",
+                                                                   spa::DesignEntityType::STMT));
+    spa::QueryResult queryResult = relationshipStorage.getAffectsStarStatementStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+    
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s1", spa::DesignEntityType::STMT));
+    secondArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "s2", spa::DesignEntityType::STMT));
+    queryResult = relationshipStorage.getAffectsStarStatementStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(expected == queryResult.getLineNumberLineNumberPairs());
+
+    firstArg = spa::PKBQueryArg(spa::PqlArgument(spa::ArgumentType::SYNONYM, "re", spa::DesignEntityType::READ));
+    queryResult = relationshipStorage.getAffectsStarStatementStatement(firstArg, secondArg);
+
+    Assert::IsTrue(queryResult.getQueryResultType() == spa::QueryResultType::TUPLE);
+    Assert::IsTrue(queryResult.getLineNumberLineNumberPairs().empty());
+  }
   };
 }  // namespace UnitTesting
